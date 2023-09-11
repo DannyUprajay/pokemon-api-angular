56858eedaa1c3956d9bb74266bd7393455b6d2ea
Danny <danny.uprajay@gmail.com>
2023-09-11 17:31:42 +0200

    pokemon api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PokemonService} from "./pokemon.service";
+import {PokemonInterface} from "./pokemon.interface";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pokemon-api';
+export class AppComponent implements OnInit {
+
+    url = 'http://vps204.tyrolium.fr/apiPokemon/index.php?controller=pokemon&task=getAll';
+
+    pokemons: PokemonInterface[] | undefined;
+    pokemonDetail: PokemonInterface | undefined;
+
+    constructor(private pokemonService: PokemonService) {
+    }
+
+    ngOnInit() {
+        this.pokemonService.getPokemon().subscribe(pokemonTranslate => {
+            this.pokemons = pokemonTranslate;
+            console.log(this.pokemons);
+        })
+    }
+
+    public form: FormGroup = new FormGroup({
+        name: new FormControl(''),
+        picture: new FormControl(''),
+        id: new FormControl(''),
+        hp: new FormControl(''),
+        cp: new FormControl(''),
+        types: new FormControl(''),
+        created: new FormControl(''),
+    });
+
+    onSubmit() {
+        let addPok = {
+            'id': 0,
+            'name': '',
+            'hp': '',
+            'cp': '',
+            'picture': '',
+            'types': '',
+            'created': Date()
+        };
+        if (this.form.valid) {
+            addPok = {
+                'id': this.form.value.id,
+                'name': this.form.value.name,
+                'picture': this.form.value.picture,
+                'hp': this.form.value.hp,
+                'cp': this.form.value.cp,
+                'types': this.form.value.types,
+                'created': Date(),
+
+            };
+            // @ts-ignore
+            this.pokemons.push(addPok);
+
+        } else {
+            console.log('nono');
+        }
+    }
+
+
+    selectPokemon(pokemon: PokemonInterface) {
+        this.pokemonDetail = pokemon;
+        console.log(this.pokemonDetail);
+    }
+
 }
+
---
+++ src/app/pokemon.interface.ts
@@ -0,0 +1,35 @@
+export interface PokemonInterface {
+  id: string,
+  hp: string,
+  cp: string,
+  name: string,
+  picture: string,
+  types: string,
+  created: string
+}
+
+/*
+
+[
+{
+"id":"2",
+"hp":"28",
+"cp":"6",
+"name":"Salam\u00e8che",
+"picture":"https:\/\/assets.pokemon.com\/assets\/cms2\/img\/pokedex\/detail\/004.png",
+"types":"Feu",
+"created":"2023-09-04 13:49:02"
+},
+
+{
+"id":"3",
+"hp":"21",
+"cp":"4",
+"name":"Carapuce",
+"picture":"https:\/\/assets.pokemon.com\/assets\/cms2\/img\/pokedex\/detail\/007.png",
+"types":"Eau",
+"created":"2023-09-04 14:28:52"
+}
+
+]
+ */
